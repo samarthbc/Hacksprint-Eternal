bb70cd5288c2792945f85d71c736e165bb5f8bf1
unknownalien1 <samarthbellam@gmail.com>
2024-10-04 14:47:07 +0530

    manyu member1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Alarm from './components/Alarm';
+import Stopwatch from './components/Stopwatch';
+import Timer from './components/Timer';
+import Pomodoro from './components/Pomodoro';
+
+const App = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Digital Clock</h1>
+      <h2>{time.toLocaleTimeString()}</h2>
+
+      <div className="flex-container">
+        <Alarm currentTime={time} />
+        <Stopwatch />
+        <Timer />
+        <Pomodoro />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
